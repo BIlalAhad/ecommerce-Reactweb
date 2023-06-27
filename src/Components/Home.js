84f1a6b31a3745b00de3102640684c26ee45efa0
Engr Bilal Ahad <79417977+BIlalAhad@users.noreply.github.com>
2023-06-27 17:37:08 +0500

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className=''>
       <div>
-        <img className="w-full  relative brightness-50  -mt-20 h-96 object-cover" src="../home.avif" alt="img" />
+        <img className="w-full  relative brightness-50  -mt-20 h-96 object-cover" src="./home.avif" alt="img" />
         <h2 className="absolute text-6xl  sm:top-56 w-full text-white font-serif text-center ">Home</h2>
       </div>
       <div className='p-3'>
